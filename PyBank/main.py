d0ed9c5fcf7d9caca4061b8b26278f895b0bb034
coreydevinanderson <coreanderson@valdosta.edu>
2021-04-03 01:04:49 -0400

    Added annotation to main.py for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,19 +11,29 @@ with open(csvpath) as csvfile:
 PL_only = []
 dates_only = []
 
+# Create separate lists for profit/losses column and dates
 for i in range(len(PL_data)):
     PL_only.append(int(PL_data[i][1]))
     dates_only.append(PL_data[i][0])
 
+# Exclude first date for change in proft/losses
 dates_only2 = dates_only[1:len(dates_only)]
-months = len(PL_only)
+
+# Total months is equal to total number of rows.
+months = len(dates_only)
+
+# Total profits/losses over all months.
 PL_sum = sum(PL_only)
+
+# Create empty list to append month change in profit/losses via for loop
 PL_diff = []
 
 for j in range(1, len(PL_only)):
     diff = PL_only[j] - PL_only[j - 1]
     PL_diff.append(diff)
 
+
+# Calculate summary statistics
 PL_change_average = "{0:.2f}".format(sum(PL_diff) / float((months - 1)))
 PL_change_max = max(PL_diff)
 PL_change_min = min(PL_diff)
